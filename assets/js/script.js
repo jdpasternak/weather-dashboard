3c76aa57739550db0d3d6c80988e6b1f0aaa8e83
Jake Pasternak <jdp.pasternak@gmail.com>
2021-11-22 16:39:39 -1000

    Add search functionality with error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 const API_KEY = "5e465552b5b1cf92efd83e5e7d35aea2";
 const DateTime = luxon.DateTime;
 
-var cityName = "Honolulu";
-
 var currentCityEl = document.getElementById("currentCity");
 var currentDateEl = document.getElementById("currentDate");
 var currentWeatherIconEl = document.getElementById("currentWeatherIcon");
@@ -11,56 +9,60 @@ var currentWindEl = document.getElementById("currentWind");
 var currentHumidityEl = document.getElementById("currentHumidity");
 var currentUvIndexEl = document.getElementById("currentUvIndex");
 var fiveDayForecastEl = document.getElementById("fiveDayForecast");
+var searchButtonEl = document.getElementById("searchButton");
+var citySearchEl = document.getElementById("citySearch");
+var forecastsEl = document.getElementById("forecasts");
 
-fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
-).then((response) => response.json().then((data) => console.log(data)));
-fetch(
-  `https://api.openweathermap.org/data/2.5/onecall?lat=21.3069&lon=-157.8583&units=imperial&exclude=hourly&appid=${API_KEY}`
-).then((response) => response.json().then((data) => console.log(data)));
+// fetch(
+//   `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+// ).then((response) => response.json().then((data) => console.log(data)));
+// fetch(
+//   `https://api.openweathermap.org/data/2.5/onecall?lat=21.3069&lon=-157.8583&units=imperial&exclude=hourly&appid=${API_KEY}`
+// ).then((response) => response.json().then((data) => console.log(data)));
 
 var getForecast = function (cityName) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
-  ).then((response) =>
-    response.json().then((data) => {
-      var currentCity = data.name;
-      fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&exclude=hourly&appid=${API_KEY}`
-      ).then((response) =>
-        response.json().then((data) => {
-          currentCityEl.innerText = currentCity;
-          currentDateEl.innerText = DateTime.fromSeconds(
-            data.current.dt
-          ).toLocaleString();
-          currentWeatherIconEl.innerHTML = `<img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png" />`;
-          currentTempEl.innerHTML = `${data.current.temp}&deg;F`;
-          currentWindEl.innerHTML = `${data.current.wind_speed} MPH`;
-          currentHumidityEl.innerHTML = `${data.current.humidity}%`;
-          var uvIndex = data.current.uvi;
-          currentUvIndexEl.innerHTML = uvIndex;
-          if (uvIndex < 5) {
-            currentUvIndexEl.classList = "badge bg-success";
-          } else if (uvIndex < 10) {
-            currentUvIndexEl.classList = "badge bg-warning";
-          } else {
-            currentUvIndexEl.classList = "badge bg-danger";
-          }
+  ).then((response) => {
+    if (response.ok) {
+      response.json().then((data) => {
+        var currentCity = data.name;
+        fetch(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&exclude=hourly&appid=${API_KEY}`
+        ).then((response) =>
+          response.json().then((data) => {
+            currentCityEl.innerText = currentCity;
+            currentDateEl.innerText = DateTime.fromSeconds(
+              data.current.dt
+            ).toLocaleString();
+            currentWeatherIconEl.innerHTML = `<img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png" />`;
+            currentTempEl.innerHTML = `${data.current.temp}&deg;F`;
+            currentWindEl.innerHTML = `${data.current.wind_speed} MPH`;
+            currentHumidityEl.innerHTML = `${data.current.humidity}%`;
+            var uvIndex = data.current.uvi;
+            currentUvIndexEl.innerHTML = uvIndex;
+            if (uvIndex < 5) {
+              currentUvIndexEl.classList = "badge bg-success";
+            } else if (uvIndex < 10) {
+              currentUvIndexEl.classList = "badge bg-warning";
+            } else {
+              currentUvIndexEl.classList = "badge bg-danger";
+            }
 
-          fiveDayForecastEl.innerHTML = "";
+            fiveDayForecastEl.innerHTML = "";
 
-          var futureForecast = data.daily;
-          for (var i = 1; i < 6; i++) {
-            var forecastInfo = futureForecast[i];
-            var date = DateTime.fromSeconds(forecastInfo.dt).toLocaleString();
-            var weatherIcon = `<img src="https://openweathermap.org/img/w/${forecastInfo.weather[0].icon}.png"`;
-            var temp = forecastInfo.temp.day;
-            var wind = forecastInfo.wind_speed;
-            var humidity = forecastInfo.humidity;
+            var futureForecast = data.daily;
+            for (var i = 1; i < 6; i++) {
+              var forecastInfo = futureForecast[i];
+              var date = DateTime.fromSeconds(forecastInfo.dt).toLocaleString();
+              var weatherIcon = `<img src="https://openweathermap.org/img/w/${forecastInfo.weather[0].icon}.png"`;
+              var temp = forecastInfo.temp.day;
+              var wind = forecastInfo.wind_speed;
+              var humidity = forecastInfo.humidity;
 
-            var futureForecastCardEl = document.createElement("div");
-            futureForecastCardEl.classList = "card col-12 col-xl-2";
-            var content = `<div class="card-body">
+              var futureForecastCardEl = document.createElement("div");
+              futureForecastCardEl.classList = "card col-12 col-xl-2";
+              var content = `<div class="card-body">
             <h6 class="card-title">${date}</h4>
             <div class="card-text">
               <div>
@@ -74,15 +76,55 @@ var getForecast = function (cityName) {
             </div>
           </div>`;
 
-            futureForecastCardEl.innerHTML = content;
-            fiveDayForecastEl.appendChild(futureForecastCardEl);
+              futureForecastCardEl.innerHTML = content;
+              fiveDayForecastEl.appendChild(futureForecastCardEl);
 
-            console.log(date, temp, wind, humidity);
-          }
-        })
-      );
-    })
-  );
+              console.log(date, temp, wind, humidity);
+            }
+          })
+        );
+      });
+    } else {
+      forecastsEl.innerHTML = "That city doesn't exists on this planet!";
+    }
+  });
 };
 
-getForecast("Honolulu");
+var searchButtonHandler = function (evt) {
+  var searchTerm = citySearchEl.value;
+  console.log(searchTerm);
+  citySearchEl.value = "";
+  searchButtonEl.classList.add("disabled");
+  location.replace(`?city=${searchTerm}`);
+};
+
+var queryHandler = () => {
+  var queryString = location.search;
+  if (queryString) {
+    queryString
+      .split("?")[1]
+      .split("&")
+      .forEach((q) => {
+        if (q.split("=")[0] === "city") {
+          getForecast(q.split("=")[1]);
+        } else {
+          forecastsEl.innerHTML = "Enter a City to view forecasts.";
+        }
+      });
+  } else {
+    forecastsEl.innerHTML = "Enter a City to view forecasts.";
+  }
+};
+
+// Disables search button if no input
+citySearchEl.addEventListener("input", () => {
+  if (citySearchEl.value) {
+    searchButtonEl.classList.remove("disabled");
+  } else {
+    searchButtonEl.classList.add("disabled");
+  }
+});
+searchButtonEl.addEventListener("click", searchButtonHandler);
+
+queryHandler();
+// getForecast("Honolulu");
